a4aa252cd6e42959de5594327e2474c21b8225ac
vladislav <v.andreev.job@gmail.com>
2016-05-24 03:08:54 +0300

    to do experements

--- numericMethod/Form1.Designer.cs
+++ numericMethod/Form1.Designer.cs
@@ -28,12 +28,259 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.labelH = new System.Windows.Forms.Label();
+            this.labelN = new System.Windows.Forms.Label();
+            this.labelM = new System.Windows.Forms.Label();
+            this.labelK = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelSP = new System.Windows.Forms.Label();
+            this.numericUpDownSBust = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownSMax = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownSMin = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownTMax = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownTMin = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownMMax = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownMMin = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownRMax = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownRMin = new System.Windows.Forms.NumericUpDown();
+            this.labelTK = new System.Windows.Forms.Label();
+            this.numericUpDownTK = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSBust)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSMax)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSMin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTMax)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTMin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMMax)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMMin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRMax)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRMin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTK)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelH
+            // 
+            this.labelH.AutoSize = true;
+            this.labelH.Location = new System.Drawing.Point(113, 17);
+            this.labelH.Name = "labelH";
+            this.labelH.Size = new System.Drawing.Size(45, 13);
+            this.labelH.TabIndex = 0;
+            this.labelH.Text = "< Шаг <";
+            // 
+            // labelN
+            // 
+            this.labelN.AutoSize = true;
+            this.labelN.Location = new System.Drawing.Point(71, 56);
+            this.labelN.Name = "labelN";
+            this.labelN.Size = new System.Drawing.Size(139, 13);
+            this.labelN.TabIndex = 2;
+            this.labelN.Text = "< Коэффициент трения <  ";
+            // 
+            // labelM
+            // 
+            this.labelM.AutoSize = true;
+            this.labelM.Location = new System.Drawing.Point(83, 95);
+            this.labelM.Name = "labelM";
+            this.labelM.Size = new System.Drawing.Size(109, 13);
+            this.labelM.TabIndex = 4;
+            this.labelM.Text = "< Масса объекта <  ";
+            // 
+            // labelK
+            // 
+            this.labelK.AutoSize = true;
+            this.labelK.Location = new System.Drawing.Point(71, 139);
+            this.labelK.Name = "labelK";
+            this.labelK.Size = new System.Drawing.Size(130, 13);
+            this.labelK.TabIndex = 6;
+            this.labelK.Text = "< Жесткость пружины <";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Location = new System.Drawing.Point(27, 244);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(466, 246);
+            this.chart1.TabIndex = 12;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(215, 187);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Вычислить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelSP
+            // 
+            this.labelSP.AutoSize = true;
+            this.labelSP.Location = new System.Drawing.Point(351, 34);
+            this.labelSP.Name = "labelSP";
+            this.labelSP.Size = new System.Drawing.Size(78, 13);
+            this.labelSP.TabIndex = 14;
+            this.labelSP.Text = "Шаг перебора";
+            // 
+            // numericUpDownSBust
+            // 
+            this.numericUpDownSBust.Location = new System.Drawing.Point(354, 53);
+            this.numericUpDownSBust.Name = "numericUpDownSBust";
+            this.numericUpDownSBust.Size = new System.Drawing.Size(75, 20);
+            this.numericUpDownSBust.TabIndex = 15;
+            this.numericUpDownSBust.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            // 
+            // numericUpDownSMax
+            // 
+            this.numericUpDownSMax.Location = new System.Drawing.Point(165, 14);
+            this.numericUpDownSMax.Name = "numericUpDownSMax";
+            this.numericUpDownSMax.Size = new System.Drawing.Size(53, 20);
+            this.numericUpDownSMax.TabIndex = 16;
+            // 
+            // numericUpDownSMin
+            // 
+            this.numericUpDownSMin.Location = new System.Drawing.Point(54, 14);
+            this.numericUpDownSMin.Name = "numericUpDownSMin";
+            this.numericUpDownSMin.Size = new System.Drawing.Size(53, 20);
+            this.numericUpDownSMin.TabIndex = 17;
+            // 
+            // numericUpDownTMax
+            // 
+            this.numericUpDownTMax.Location = new System.Drawing.Point(210, 53);
+            this.numericUpDownTMax.Name = "numericUpDownTMax";
+            this.numericUpDownTMax.Size = new System.Drawing.Size(53, 20);
+            this.numericUpDownTMax.TabIndex = 18;
+            // 
+            // numericUpDownTMin
+            // 
+            this.numericUpDownTMin.Location = new System.Drawing.Point(12, 54);
+            this.numericUpDownTMin.Name = "numericUpDownTMin";
+            this.numericUpDownTMin.Size = new System.Drawing.Size(53, 20);
+            this.numericUpDownTMin.TabIndex = 19;
+            // 
+            // numericUpDownMMax
+            // 
+            this.numericUpDownMMax.Location = new System.Drawing.Point(198, 93);
+            this.numericUpDownMMax.Name = "numericUpDownMMax";
+            this.numericUpDownMMax.Size = new System.Drawing.Size(53, 20);
+            this.numericUpDownMMax.TabIndex = 20;
+            // 
+            // numericUpDownMMin
+            // 
+            this.numericUpDownMMin.Location = new System.Drawing.Point(27, 93);
+            this.numericUpDownMMin.Name = "numericUpDownMMin";
+            this.numericUpDownMMin.Size = new System.Drawing.Size(53, 20);
+            this.numericUpDownMMin.TabIndex = 21;
+            // 
+            // numericUpDownRMax
+            // 
+            this.numericUpDownRMax.Location = new System.Drawing.Point(207, 136);
+            this.numericUpDownRMax.Name = "numericUpDownRMax";
+            this.numericUpDownRMax.Size = new System.Drawing.Size(53, 20);
+            this.numericUpDownRMax.TabIndex = 22;
+            // 
+            // numericUpDownRMin
+            // 
+            this.numericUpDownRMin.Location = new System.Drawing.Point(12, 137);
+            this.numericUpDownRMin.Name = "numericUpDownRMin";
+            this.numericUpDownRMin.Size = new System.Drawing.Size(53, 20);
+            this.numericUpDownRMin.TabIndex = 23;
+            // 
+            // labelTK
+            // 
+            this.labelTK.AutoSize = true;
+            this.labelTK.Location = new System.Drawing.Point(322, 99);
+            this.labelTK.Name = "labelTK";
+            this.labelTK.Size = new System.Drawing.Size(127, 13);
+            this.labelTK.TabIndex = 24;
+            this.labelTK.Text = "Задача коши, введите х";
+            // 
+            // numericUpDownTK
+            // 
+            this.numericUpDownTK.Location = new System.Drawing.Point(354, 132);
+            this.numericUpDownTK.Name = "numericUpDownTK";
+            this.numericUpDownTK.Size = new System.Drawing.Size(75, 20);
+            this.numericUpDownTK.TabIndex = 25;
+            this.numericUpDownTK.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 496);
+            this.Controls.Add(this.numericUpDownTK);
+            this.Controls.Add(this.labelTK);
+            this.Controls.Add(this.numericUpDownRMin);
+            this.Controls.Add(this.numericUpDownRMax);
+            this.Controls.Add(this.numericUpDownMMin);
+            this.Controls.Add(this.numericUpDownMMax);
+            this.Controls.Add(this.numericUpDownTMin);
+            this.Controls.Add(this.numericUpDownTMax);
+            this.Controls.Add(this.numericUpDownSMin);
+            this.Controls.Add(this.numericUpDownSMax);
+            this.Controls.Add(this.numericUpDownSBust);
+            this.Controls.Add(this.labelSP);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.labelK);
+            this.Controls.Add(this.labelM);
+            this.Controls.Add(this.labelN);
+            this.Controls.Add(this.labelH);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSBust)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSMax)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSMin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTMax)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTMin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMMax)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMMin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRMax)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRMin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTK)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelH;
+        private System.Windows.Forms.Label labelN;
+        private System.Windows.Forms.Label labelM;
+        private System.Windows.Forms.Label labelK;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelSP;
+        private System.Windows.Forms.NumericUpDown numericUpDownSBust;
+        private System.Windows.Forms.NumericUpDown numericUpDownSMax;
+        private System.Windows.Forms.NumericUpDown numericUpDownSMin;
+        private System.Windows.Forms.NumericUpDown numericUpDownTMax;
+        private System.Windows.Forms.NumericUpDown numericUpDownTMin;
+        private System.Windows.Forms.NumericUpDown numericUpDownMMax;
+        private System.Windows.Forms.NumericUpDown numericUpDownMMin;
+        private System.Windows.Forms.NumericUpDown numericUpDownRMax;
+        private System.Windows.Forms.NumericUpDown numericUpDownRMin;
+        private System.Windows.Forms.Label labelTK;
+        private System.Windows.Forms.NumericUpDown numericUpDownTK;
     }
 }
 
--- numericMethod/Form1.cs
+++ numericMethod/Form1.cs
@@ -15,5 +15,112 @@ namespace numericMethod
         {
             InitializeComponent();
         }
+        
+        PointF point = new Point();
+        List<PointF> minPoints = new List<PointF>();
+
+        int countStepsOfSizeStep, countStepsOfFriction, countStepsOfWeight, countStepsOfRigidity;
+
+        private double first_function(double y)
+        {  // Первая функция
+            return y;
+        }
+
+        private double second_function(double k, double m, double M, double x, double y)
+        { //Вторая функция
+            return -(k / m) * x - (M / m) * y;
+        }
+        private void runge_kutta_method(double h, double k, double m, double M)
+        {	
+            double k1,k2,k3,k4; // Коэффициенты метода для первой функции
+            double m1,m2,m3,m4;
+            double t = 0;
+            double x = point.X;
+            double y = point.Y;
+            int count = 0;
+                count++;
+
+                k1 = h * first_function(y);
+                m1 = h * second_function(k, m, M, x, y);
+
+                k2 = h * first_function(y + k1 / 2);
+                m2 = h * second_function(k, m, M, x + h / 2, y + (h * m1) / 2);
+
+                k3 = h * first_function(y + k2 / 2);
+                m3 = h * second_function(k, m, M, x + h / 2, y + (h * m2) / 2);
+
+                k4 = h * first_function(y + h * k3);
+                m4 = h * second_function(k, m, M, x + h, y + h * m3);
+
+                point.X += (float)((h / 6) * (k1 + 2 * k2 + 2 * k3 + k4));
+                point.Y += (float)((h / 6) * (m1 + 2 * m2 + 2 * m3 + m4));
+
+                t += h;
+            
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            point.X = 5;
+            point.Y = -0.0001f;
+            minPoints.Add(new PointF(0, 10));
+            minPoints.Add(new PointF(1000, 10));
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+        private void computePloatWithNecessaryParameters(float h, float k, float m, float M)
+        {
+            chart1.Series[0].Points.Clear();
+            List<PointF> currentPoints = new List<PointF>();
+            while (point.Y < 0.0)
+            {
+                runge_kutta_method(h, k, m, M);
+                currentPoints.Add(point);
+                //chart1.Series[0].Points.AddXY(point.X, point.Y);
+            }
+            while (point.Y > 0.0)
+            {
+                runge_kutta_method(h, k, m, M);
+                currentPoints.Add(point);
+                //chart1.Series[0].Points.AddXY(point.X, point.Y);
+            }
+            if (checkMinDeltaXValue(currentPoints, minPoints))
+            {
+                minPoints = currentPoints;
+            }
+            point.X = Convert.ToInt32(numericUpDownTK.Value);
+            point.Y = -0.0001f;
+
+        }
+        private bool checkMinDeltaXValue(List<PointF> currentPoints, List<PointF> minPoints)
+        {
+            float currentDeltaX = currentPoints.ElementAt(currentPoints.Count - 1).X - currentPoints.ElementAt(0).X;
+            float minDeltaX = minPoints.ElementAt(minPoints.Count - 1).X - minPoints.ElementAt(0).X;
+            if (currentDeltaX < minDeltaX)
+            {
+                return true;
+            }
+            return false;
+        }
+        private int calculateCountExperiment()
+        {
+             countStepsOfSizeStep = (Convert.ToInt32(numericUpDownSMax.Value) - Convert.ToInt32(numericUpDownSMin.Value)) / Convert.ToInt32(numericUpDownSBust.Value);
+             countStepsOfFriction = (Convert.ToInt32(numericUpDownTMax.Value) - Convert.ToInt32(numericUpDownTMin.Value)) / Convert.ToInt32(numericUpDownSBust.Value);
+             countStepsOfWeight = (Convert.ToInt32(numericUpDownMMax.Value) - Convert.ToInt32(numericUpDownMMin.Value)) / Convert.ToInt32(numericUpDownSBust.Value);
+             countStepsOfRigidity = (Convert.ToInt32(numericUpDownRMax.Value) - Convert.ToInt32(numericUpDownRMin.Value)) / Convert.ToInt32(numericUpDownSBust.Value);
+            return countStepsOfSizeStep + countStepsOfFriction + countStepsOfWeight + countStepsOfRigidity;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            computePloatWithNecessaryParameters(0.1f, 1.8f, 5.0f, 0.2f);
+            computePloatWithNecessaryParameters(0.2f, 2, 5, 0.2f);
+            for (int i = 0; i < minPoints.Count; i++)
+            {
+                chart1.Series[0].Points.AddXY(minPoints.ElementAt(i).X, minPoints.ElementAt(i).Y);
+            }
+
+        }
     }
 }
